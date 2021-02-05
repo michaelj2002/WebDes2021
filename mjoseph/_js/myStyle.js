487f883196bec0693737b1744432f70268f94291
michaelj2002 <habsmcj@gmail.com>
2021-02-05 13:15:21 -0800

    Learning canvas
    Adding canvas to shapes

--- mjoseph/_js/myStyle.js
+++ mjoseph/_js/myStyle.js
@@ -1,9 +1,51 @@
-// add a canvas to the page
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+//test code
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);
-function drawSquare(){
-    
-}
+// let myNum = 6;
+// const myName = "Chris";
+// alert(myName);
+// alert(myNum);
+// myNum = 25;
+// alert(myNum);
+
+// add a canvas to the page
+
+
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+// ctx.fillStyle = 'green';
+// ctx.fillRect(10, 10, 150, 100);
+
+// let's write a program that creates rock paper scissors logic using circle, square, triangle
+
+//initializing variables to create a canvas 
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+//init a function to draw a triangle on the canvas
+function drawTriangle() {
+      ctx.beginPath();
+      ctx.moveTo(75, 50);
+      ctx.lineTo(100, 75);
+      ctx.lineTo(100, 25);
+      ctx.fill();
+    }
+//init a function to draw a square on the canvas
+  function drawSquare(){
+    ctx.fillRect(25, 25, 100, 100);
+    ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(50, 50, 50, 50);
+  }
+  //init a function to draw a circle on the canvas
+  function drawCircle(){
+    //ctx.beginPath();
+    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  function main(){
+    drawTriangle();
+    drawSquare();
+    drawCircle();
+  }
+
+main();
